323fb338669c8e2584a2086f163eb0ef72b1a842
John Pedroza <Mr.John.Pedroza@gmail.com>
2015-09-22 10:02:08 -0600

    hardcoded password string created to implement

--- index.php
+++ index.php
@@ -6,21 +6,22 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 <!DOCTYPE html>
 <html>
 <head>
-
 <meta charset='utf-8'>
 <title>Password Generator</title>
-
-<?php 
-$hello="John";
-?>
+	<?php 
+	//the following variables are to setup a dummy password string to construct 
+		$word ="secret";
+		$specialcharacter ="*";
+		$number =5;
+	?>
 
 </head>
 <body>
-<h1> Password Successfully Generated 4! </h1>
+<h1> Password Successfully Generated</h1>
 
-<p> 
-Your password is <?php echo $hello; ?> once before
-</p> 
+	<p> 
+	Your password is <?php echo $word . $specialcharacter . $number; ?>
+	</p> 
 
 </body>
 </html>
