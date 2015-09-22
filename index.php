7d6b12a5289b373b2f0dd63e9ffe51edc7dee4ca
John Pedroza <Mr.John.Pedroza@gmail.com>
2015-09-22 12:42:57 -0600

    adding form input reading and validation with functions in the logic.php

--- index.php
+++ index.php
@@ -14,6 +14,23 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 
 <body>
 <h1> Password Successfully Generated</h1>
+<form method='POST' action='index.php'>
+    <input type='number' name='intWords' min="1" max="10">How many words do you want (e.g., 1 to 10)?<br>
+    <input type='number' name='intSpecialCharacters' min="1" max="10">How many Special Characters do you want (e.g., 1 to 10)?<br>
+    <input type='number' name='intNumbers' min="1" max="10">How many Numbers do you want (e.g., 1 to 10)?<br>	
+    <input type='number' name='intTotalCharacters' min="1" max="40">How many Total Characters do you want (e.g., 1 to 40)?<br>	
+    <input type='submit' value='Generate'><br>
+</form>
+<pre>
+    <?php 
+
+		echo "This is the value passed: " . $_POST['intWords']; echo "<br>";
+		echo "This is the value passed: " . $_POST['intSpecialCharacters']; echo "<br>";	
+		echo "This is the value passed: " . $_POST['intNumbers']; echo "<br>";	
+		echo "This is the value passed: " . $_POST['intTotalCharacters']; echo "<br>";			
+	?>
+</pre>
+
 
 	<p> 
 	Your password is <?php echo $password; ?>
